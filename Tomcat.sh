e9e3e40564bc4024d9cf1ba9fadd774f99243b76
ashwini <53709167+ashwini1331@users.noreply.github.com>
2021-11-30 12:33:18 +0530

    Update Tomcat.sh
    update machine 16 ubuntu required

--- Tomcat.sh
+++ Tomcat.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get -y install tomcat7 tomcat7-admin lynx
-sudo mv /etc/tomcat7/tomcat-users.xml /etc/tomcat7/tomcat-users.xml.bak
+sudo apt-get -y install tomcat7 tomcat7-admin 
+sudo rm -f /etc/tomcat7/tomcat-users.xml
 wget https://raw.githubusercontent.com/ashwini1331/Tomcat-installation/main/user.xml -O /etc/tomcat7/tomcat-users.xml
 sudo service tomcat7 restart
