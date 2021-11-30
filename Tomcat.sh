ada0f1d2fee986acca9c525d28b86e340413dfa2
ashwini <53709167+ashwini1331@users.noreply.github.com>
2021-11-30 12:33:57 +0530

    Update Tomcat.sh

--- Tomcat.sh
+++ Tomcat.sh
@@ -1,5 +1,6 @@
-sudo apt-get update
+sudo apt-get update -y 
 sudo apt-get -y install tomcat7 tomcat7-admin 
+sudo apt-get update -y 
 sudo rm -f /etc/tomcat7/tomcat-users.xml
 wget https://raw.githubusercontent.com/ashwini1331/Tomcat-installation/main/user.xml -O /etc/tomcat7/tomcat-users.xml
 sudo service tomcat7 restart
